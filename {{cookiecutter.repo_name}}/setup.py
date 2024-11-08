3913965ad081c75853abfd552b3106c322becd5e
Dani Zysman <dani.zysman@gmail.com>
2024-11-08 17:59:05 -0300

    customized structure

--- {{cookiecutter.repo_name}}/setup.py
+++ {{cookiecutter.repo_name}}/setup.py
@@ -1,6 +1,23 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="{{ cookiecutter.package_name }}",
+    name='src',
+    version='0.1.0',
+    description='Udemy Clips',
+    author='Daniel Zysman',
+    author_email='dani@workera.ai',
+    url='https://github.com/yourusername/yourproject',
     packages=find_packages(),
+    install_requires=[
+        # List your project dependencies here
+        'pandas',
+        'numpy',
+        # Add other dependencies as needed
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
